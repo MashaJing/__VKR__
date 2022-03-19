a7d223037f55a421edb787bf222f35873d8c49fc
Мария Дзех <mdzekh23032000a@gmail.com>
2022-03-19 17:31:46 +0700

    Added interface draft

---
+++ GUI.py
@@ -0,0 +1,93 @@
+import PySimpleGUI as sg
+import os.path
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib
+
+fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
+t = np.arange(0, 3, .01)
+fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+matplotlib.use("TkAgg")
+
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
+    return figure_canvas_agg
+# First the window layout in 2 columns
+
+file_list_column = [
+    [
+        sg.Text("Папка с данными ЭЭГ"),
+        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse(),
+    ],
+    [
+        sg.Listbox(
+            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+        )
+    ],
+]
+
+# For now will only show the name of the file that was chosen
+image_viewer_column = [
+    [sg.Text("Выберите edf-файл:")],
+    [sg.Text(size=(40, 1), key="-TOUT-")],
+    [sg.Canvas(key="-CANVAS-")],
+]
+
+# ----- Full layout -----
+layout = [
+    [
+        sg.Column(file_list_column),
+        sg.VSeperator(),
+        sg.Column(image_viewer_column),
+    ]
+]
+
+window = sg.Window(
+    "EGG Viewer",
+    layout, 
+    location=(0, 0),
+    finalize=True,
+    element_justification="center",
+    font="Helvetica 18",
+)
+# Add the plot to the window
+# todo: move to loop and flush every time
+draw_figure(window["-CANVAS-"].TKCanvas, fig)
+
+# Run the Event Loop
+while True:
+    event, values = window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+    # Folder name was filled in, make a list of files in the folder
+    if event == "-FOLDER-":
+        folder = values["-FOLDER-"]
+        try:
+            # Get list of files in folder
+            file_list = os.listdir(folder)
+        except:
+            file_list = []
+
+        fnames = [
+            f
+            for f in file_list
+            if os.path.isfile(os.path.join(folder, f))
+            and f.lower().endswith((".edf", ".EDF"))
+        ]
+        window["-FILE LIST-"].update(fnames)
+    elif event == "-FILE LIST-":  # A file was chosen from the listbox
+        try:
+            filename = os.path.join(
+                values["-FOLDER-"], values["-FILE LIST-"][0]
+            )
+            window["-TOUT-"].update(filename)
+            # window["-IMAGE-"].update(filename=filename)
+
+        except:
+            pass
+    
+    
+window.close()
--- egg_test.py
+++ egg_test.py
@@ -1,6 +1,31 @@
 import eegraph
 
+bands = ['alpha','beta','gamma','delta','theta']
+# for i in bands:
 G = eegraph.Graph()
-G.load_data(path = 'data\\S001R01.edf', exclude = ['EEG TAntI1-TAntI', 'EEG TAntD1-TAntD', 'EEG EKG1-EKG2'])
-graphs, connectivity_matrix = G.modelate(window_size = 2, connectivity = 'squared_coherence', bands = ['delta','theta','alpha'])
-print(connectivity_matrix)
+G.load_data(path = 'data/S001R01.edf', exclude = [
+'Fc5.', 'Fc3.', 'Fc1.', 'Fcz.', 'Fc2.',
+'Fc4.', 'Fc6.', 'C5..', 'C3..', 'C1..',
+        'C2..', 'C4..', 'C6..', 'Cp5.',
+'Cp3.', 'Cp1.', 'Cpz.', 'Cp2.', 'Cp4.',
+'Fp1.', 'Fpz.', 'Fp2.', 'Af7.', 'Af3.',
+'Afz.', 'Af4.', 'Af8.', 'F7..', 'F5..',
+'F3..', 'F1..',         'F2..', 'F4..', 
+'F6..', 'F8..', 'Ft7.', 'Ft8.', 'T7..', 
+'T8..', 'T9..', 'T10.', 'Tp7.', 'Cp6.',
+'Tp8.', 'P7..', 'P5..', 'P3..', 'P1..', 
+'P2..', 'P4..', 'P6..', 'P8..', 'Po7.',
+'Po3.', 'Poz.', 'Po4.', 'Po8.',   
+'O1..',         'O2..', 'Iz..'])
+print(G.ch_names)
+# G.load_data(path = 'data/S001R01.edf', exclude = ['EEG TAntI1-TAntI', 'EEG TAntD1-TAntD', 'EEG EKG1-EKG2'])
+graphs, connectivity_matrix = G.modelate(window_size = 200, connectivity = 'squared_coherence', bands=bands)
+G.visualize(graphs[0], f'graph_1')
+
+"""
+1. Какие электроды необходимо исключать?
+2. Написать даме, чтоб кинула скрипт
+3. Построить какой-нибудь уже граф
+"""
+
+# standard_electrodes = ['Cz', 'Pz', 'Oz', 'Fz', 'Nz']
